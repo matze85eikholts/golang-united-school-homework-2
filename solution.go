b8f005227850fc62785995c9af3b52faea1de53b
unknown <matze85eikholts@gmail.com>
2022-07-02 11:24:12 +0300

    Solution of the second task related to the function of sqare counting

--- solution.go
+++ solution.go
@@ -1,5 +1,11 @@
 package square
 
+import "math"
+
+const SidesSquare = 4
+const SidesTriangle = 3
+const SidesCircle = 0
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +14,14 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum int64) float64 {
+	if sidesNum == int64(SidesSquare) {
+		return sideLen * sideLen
+	} else if sidesNum == int64(SidesTriangle) {
+		return (float64(math.Sqrt(3) / 4)) * sideLen * sideLen
+	} else if sidesNum == int64(SidesCircle) {
+		return (float64(math.Pi)) * sideLen * sideLen
+	} else {
+		return 0.0
+	}
 }
